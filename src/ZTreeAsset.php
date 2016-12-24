1158f98194fa6b1998a57ab5bb1982c8dc9eff40
Rico yao <ailiangkuai@qq.com>
2016-12-24 14:54:55 +0800

    project init

---
+++ src/ZTreeAsset.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @copyright Copyright qrCms
+ * User: Rico yao <ailiangkuai@qq.com>
+ * Date: 2016/12/22
+ */
+
+namespace ailiangkuai\yii2\widgets\layer;
+
+
+use yii\web\AssetBundle;
+
+class ZTreeAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/ailiangkuai/yii2-ztree/src/assets';
+    public $js = [
+        'js/jquery.ztree.core.min.js'
+    ];
+    public $css = [
+        'css/zTreeStyle/zTreeStyle.css'
+    ];
+}
